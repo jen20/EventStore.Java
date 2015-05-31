7b6372b80a892c500047742879ede01d2ef25203
James Nugent <james@jen20.com>
2015-05-31 15:37:22 -0400

    Make tests read data from resources

---
+++ src/test/java/com/jen20/client/internal/TestDataResources.java
@@ -0,0 +1,30 @@
+package com.jen20.client.internal;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import static org.junit.Assert.*;
+
+public class TestDataResources {
+    private static ByteBuf resourceAsByteBuf(String resourceName) {
+        try {
+            URL resource = TestDataResources.class.getResource(resourceName);
+            InputStream resourceStream = resource.openStream();
+
+            ByteBuf buf = Unpooled.buffer(resourceStream.available());
+            buf.writeBytes(resourceStream, resourceStream.available());
+            return buf;
+        } catch (IOException e) {
+            assertTrue("Cannot read test resource " + resourceName, false);
+            return null;
+        }
+    }
+
+    public static ByteBuf singleLengthPrefixedTcpPackage() {
+        return resourceAsByteBuf("/singleLengthPrefixedTcpPackage");
+    }
+}
